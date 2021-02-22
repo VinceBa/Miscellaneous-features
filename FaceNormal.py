5e0dc729190eab4210192409c7520283652cb1a3
VinceBa <77238155+VinceBa@users.noreply.github.com>
2021-02-22 19:51:44 +0100

    Add files via upload

--- FaceNormal.py
+++ FaceNormal.py
@@ -1,4 +1,4 @@
-
+#
 # A python feature make a normal vector on a surface
 # wrote by Vincent Ballu
 # any remarks: vincent.ballu@gmx.com
@@ -8,9 +8,7 @@
 #01/01/2021 first release
 #21/02/2021 make a cone at the vector end
 #           start on the surface instead the center mass
-
-
-
+#22/02/2021 check if select object is a face
 
 from __future__ import division
 import FreeCAD
@@ -18,13 +16,12 @@ import Part
 
 class Normal:
     def __init__(self, obj, face):
-        obj.addProperty("App::PropertyFloat","Length","Parameters","Lenght of the edge").Length = 1.0
+        obj.addProperty("App::PropertyInteger","Length","Parameters","Lenght of the edge").Length = 5
         obj.addProperty("App::PropertyLinkSub","Target","Parameters","Target face").Target = face
         obj.Proxy = self
 
     def onChanged(self, obj, prop):
-        if prop == "Length":
-            self.execute(obj)
+        if prop == "Length":   self.execute(obj)
 
     def execute(self, obj):
         f = obj.Target[0].getSubObject(obj.Target[1][0])
@@ -40,16 +37,20 @@ class Normal:
         obj.Shape = comp
 
 def MakeNormal():
-    doc = FreeCAD.activeDocument()
-    obj = doc.addObject("Part::FeaturePython","FaceNormal") 
     try:
         f = Gui.Selection.getSelectionEx()[0]
-        linksub = (f.Object, (f.SubElementNames[0]))
-        Normal(obj, linksub)
-        obj.ViewObject.Proxy = 0
-        viewObject = Gui.ActiveDocument.getObject(obj.Name)
-        viewObject.DisplayMode = "Flat Lines"
+        sub = f.SubElementNames[0]
+        linksub = (f.Object, sub)
+        if sub[0:4] == "Face":
+            doc = FreeCAD.activeDocument()
+            obj = doc.addObject("Part::FeaturePython","FaceNormal")           
+            Normal(obj, linksub)
+            obj.ViewObject.Proxy = 0
+            # viewObject = Gui.ActiveDocument.getObject(obj.Name)
+            # viewObject.DisplayMode = "Flat Lines"
+        else:
+            print("Select a face, then run the macro.")
     except:
-        print("Select a face, then run the macro.")
+        print("Select something, then run the macro.")
         
 MakeNormal()
